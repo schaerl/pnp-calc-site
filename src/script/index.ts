2b3f2c3d49f6f9fc17696b648ef37066031d387c
Lukas Schär <luki.schaer@hispeed.ch>
2023-04-18 23:21:41 +0200

    Achieve working state with library package and compilation,transpilation etc

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,1 +1,22 @@
-console.log("Test")
+import { CharacterStats, PnpSimulator, Talent, TalentValues, defaultTalents } from "pnp-calc";
+
+const simulator = new PnpSimulator();
+
+function initDOM() {
+    createSkillFields();
+    createTalentFields();
+}
+
+function createSkillFields() {
+    for (let stat of Object.values(CharacterStats).filter(x => typeof x === "string")) {
+        console.log(stat);
+    }
+}
+
+function createTalentFields() {
+    for (let talent of Object.keys(defaultTalents)) {
+        console.log(talent);
+    }
+}
+
+initDOM()
